634a969bf05b7c983e8cedea0ba6e62c0d766df8
voxty <135042947+voxty@users.noreply.github.com>
2024-06-30 13:59:59 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'github.com/voxty'
+description 'A script that links your discord server nickname to your FiveM nickname, and checks if you are in Teamspeak or not.'
+version '1.0'
+
+lua54 "yes"
+
+client_scripts {
+
+}
+
+server_scripts {
+    
+}
